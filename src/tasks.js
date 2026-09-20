0b2538ce871ff9670e36f6e6e81500dd3083bc1e
IT24102257 <it24102257@my.sliit.lk>
2026-09-20 23:24:45 +0530

    feat: add due dates to tasks Closes #2

--- src/tasks.js
+++ src/tasks.js
@@ -1,8 +1,22 @@
 // CampusEats task list
+
 const tasks = [
-  "Design the menu screen",
-  "Build the orders API",
-  "Add user login",
+  {
+    title: "Design the menu screen",
+    dueDate: "2026-09-25",
+  },
+  {
+    title: "Build the orders API",
+    dueDate: "2026-09-28",
+  },
+  {
+    title: "Add user login",
+    dueDate: "2026-09-30",
+  },
 ];
 
 console.log(`CampusEats has ${tasks.length} open tasks`);
+
+tasks.forEach((task) => {
+  console.log(`${task.title} - Due: ${task.dueDate}`);
+});
